f3a39d97ac5e257bf8a6f654549f3b06005ab172
Alexander-2049 <a.alexander.2049@gmail.com>
2023-11-21 02:18:10 +0200

    schedule fetching + caching

---
+++ src/utils/scheduleFetch.ts
@@ -0,0 +1,42 @@
+// scheduleUtils.ts
+import { Lecture } from "../types/Lecture";
+import { RouterProps } from "../types/RouterProps";
+import getScheduleFromHTML from "./getScheduleFromHTML";
+import scheduleUnminify from "./scheduleUnminify";
+
+interface IFetchAndHandleScheduleResult {
+  schedule: Lecture[];
+  updated: boolean;
+  lastScheduleFetch: string;
+}
+
+export async function scheduleFetch(props: RouterProps): Promise<IFetchAndHandleScheduleResult> {
+  const { env } = props;
+  const { STORAGE } = env;
+  const lastScheduleFetch = await STORAGE.get('lastScheduleFetch');
+  if (lastScheduleFetch) {
+    const cachedLastScheduleFetch = new Date(lastScheduleFetch);
+    const now = new Date();
+    const diff = now.getTime() - cachedLastScheduleFetch.getTime();
+    const minutes = Math.floor(diff / 1000 / 60);
+    if (minutes < 45) {
+      const schedule = await STORAGE.get('schedule');
+      if (schedule) {
+        const cachedSchedule = scheduleUnminify(JSON.parse(schedule));
+        return {schedule: cachedSchedule, updated: false, lastScheduleFetch: lastScheduleFetch};
+      }
+    }
+  }
+
+  const api = props.api;
+  const result = await api.fetchSchedule();
+  const html = await result.text();
+  const json = getScheduleFromHTML(html);
+
+  const schedule = scheduleUnminify(json);
+  await STORAGE.put('schedule', JSON.stringify(schedule));
+  const now = new Date().toISOString();
+  await STORAGE.put('lastScheduleFetch', now);
+
+  return {schedule: schedule, updated: true, lastScheduleFetch: now};
+}
