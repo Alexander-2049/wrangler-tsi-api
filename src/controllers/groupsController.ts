2a510b5b00de81cb36faa3ac1ab5b03259dcc04c
Alexander-2049 <a.alexander.2049@gmail.com>
2023-11-22 00:10:20 +0200

    added controller for groups route

--- src/controllers/groupsController.ts
+++ src/controllers/groupsController.ts
@@ -1,6 +1,12 @@
 import { IRequest } from "itty-router";
 import { RouterProps } from "../types/RouterProps";
+import { scheduleFetch } from "../utils/scheduleFetch";
 
-export default function groupsController(request: IRequest, props: RouterProps) {
-
+export default async function groupsController(request: IRequest, props: RouterProps) {
+  const groups = await props.env.STORAGE.get('groups');
+  if(!groups) {
+    const { groups } = await scheduleFetch(props);
+    return new Response(JSON.stringify(groups), { status: 200, headers: { 'Content-Type': 'application/json' } });
+  }
+  return new Response(groups, { status: 200, headers: { 'Content-Type': 'application/json' } });
 }
