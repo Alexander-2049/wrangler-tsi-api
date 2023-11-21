0d64623d2c88b172c5e84d438bcee4c23225d9e3
Alexander-2049 <a.alexander.2049@gmail.com>
2023-11-22 00:10:50 +0200

    added query parameters "from" and "to" for schedule

--- src/controllers/scheduleController.ts
+++ src/controllers/scheduleController.ts
@@ -1,15 +1,38 @@
 import { IRequest } from "itty-router";
 import { RouterProps } from "../types/RouterProps";
 import { scheduleFetch } from "../utils/scheduleFetch";
+import getDate from "../utils/getDate";
 
 export default async function scheduleController(request: IRequest, props: RouterProps) {
   const { schedule } = await scheduleFetch(props);
 
+  let result = schedule;
+
   if(request.params.group) {
     const group = request.params.group.toUpperCase();
-    const groupSchedule = schedule.filter((item) => item.groups.includes(group));
-    return new Response(JSON.stringify(groupSchedule), { status: 200, headers: { 'Content-Type': 'application/json' } });
+    result = schedule.filter((item) => item.groups.includes(group));
   }
 
-  return new Response(JSON.stringify(schedule), { status: 200, headers: { 'Content-Type': 'application/json' } });
+  const from = typeof request.query.from === 'string' ? getDate(request.query.from) : undefined;
+  const to = typeof request.query.to === 'string' ? getDate(request.query.to) : undefined;
+
+  if(from) {
+    result = result.filter((item) => {
+      const date = getDate(item.date);
+      if(!date) return false;
+      if(date >= from) return true;
+      if(date < from) return false;
+    });
+  }
+
+  if(to) {
+    result = result.filter((item) => {
+      const date = getDate(item.date);
+      if(!date) return false;
+      if(date <= to) return true;
+      if(date > to) return false;
+    });
+  }
+
+  return new Response(JSON.stringify(result), { status: 200, headers: { 'Content-Type': 'application/json' } });
 }
