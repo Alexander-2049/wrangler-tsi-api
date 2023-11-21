9956e948ba0a625ad6f4ea993e43be2a9d81fd18
Alexander-2049 <a.alexander.2049@gmail.com>
2023-11-21 02:16:01 +0200

    added /fetch /schedule + :/group

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,13 @@
 import { Router } from 'itty-router';
 import { RouterProps } from './types/RouterProps';
 import fetchController from './controllers/fetchController';
+import scheduleController from './controllers/scheduleController';
 
-// now let's create a router (note the lack of "new")
 const router = Router();
 
-// GET collection index
 router.get('/fetch', async (request, props: RouterProps) => await fetchController(request, props));
-
-// GET item
-router.get('/todos/:id', ({ params }) => new Response(`Todo #${params.id}`));
-
-// POST to the collection (we'll use async here)
-router.post('/todos', async (request) => {
-	const content = await request.json();
-
-	return new Response('Creating Todo: ' + JSON.stringify(content));
-});
-
-router.get('/env', (request, props: RouterProps) => new Response(props.env.LOGIN + " " + props.env.PASSWORD));
+router.get('/schedule', async (request, props: RouterProps) => await scheduleController(request, props));
+router.get('/schedule/:group', async (request, props: RouterProps) => await scheduleController(request, props));
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
