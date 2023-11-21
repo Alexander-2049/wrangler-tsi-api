6aadfa01ee925ace61d641022eb333ea3c13bbd5
Alexander-2049 <a.alexander.2049@gmail.com>
2023-11-22 00:08:53 +0200

    added groups route

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,14 @@ import { Router } from 'itty-router';
 import { RouterProps } from './types/RouterProps';
 import fetchController from './controllers/fetchController';
 import scheduleController from './controllers/scheduleController';
+import groupsController from './controllers/groupsController';
 
 const router = Router();
 
 router.get('/fetch', async (request, props: RouterProps) => await fetchController(request, props));
 router.get('/schedule', async (request, props: RouterProps) => await scheduleController(request, props));
 router.get('/schedule/:group', async (request, props: RouterProps) => await scheduleController(request, props));
+router.get('/groups', async (request, props: RouterProps) => await groupsController(request, props));
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
