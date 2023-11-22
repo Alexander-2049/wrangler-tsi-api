b7a49170165615293fead3b8507525a64affb64e
Alexander-2049 <a.alexander.2049@gmail.com>
2023-11-22 22:48:02 +0200

    refactored using ChatGPT

--- src/utils/scheduleFetch.ts
+++ src/utils/scheduleFetch.ts
@@ -1,4 +1,3 @@
-// scheduleUtils.ts
 import { Lecture } from "../types/Lecture";
 import { RouterProps } from "../types/RouterProps";
 import getScheduleFromHTML from "./getScheduleFromHTML";
@@ -12,36 +11,44 @@ interface IFetchAndHandleScheduleResult {
   groups: string[];
 }
 
-export async function scheduleFetch(props: RouterProps): Promise<IFetchAndHandleScheduleResult> {
-  const { env } = props;
-  const { STORAGE } = env;
+async function getCachedScheduleAndGroups(STORAGE: any): Promise<IFetchAndHandleScheduleResult | undefined> {
   const lastScheduleFetch = await STORAGE.get('lastScheduleFetch');
+  
   if (lastScheduleFetch) {
     const cachedLastScheduleFetch = new Date(lastScheduleFetch);
     const now = new Date();
     const diff = now.getTime() - cachedLastScheduleFetch.getTime();
     const minutes = Math.floor(diff / 1000 / 60);
+    
     if (minutes < 45) {
       const schedule = await STORAGE.get('schedule');
       const groups = await STORAGE.get('groups');
+      
       if (schedule && groups) {
         const cachedSchedule = scheduleUnminify(JSON.parse(schedule));
-        return {schedule: cachedSchedule, groups: JSON.parse(groups), updated: false, lastScheduleFetch: lastScheduleFetch};
+        return { schedule: cachedSchedule, groups: JSON.parse(groups), updated: false, lastScheduleFetch: lastScheduleFetch };
       }
     }
   }
 
-  const api = props.api;
+  return undefined;
+}
+
+async function fetchScheduleFromAPI(api: any): Promise<Lecture[]> {
   const result = await api.fetchSchedule();
   const html = await result.text();
-  const schedule = getScheduleFromHTML(html);
+  return getScheduleFromHTML(html);
+}
 
+async function saveScheduleToStorage(STORAGE: any, schedule: Lecture[], now: string): Promise<void> {
   const scheduleMinified = scheduleMinify(schedule);
   await STORAGE.put('schedule', JSON.stringify(scheduleMinified));
-  const now = new Date().toISOString();
   await STORAGE.put('lastScheduleFetch', now);
+}
 
+async function saveGroupsToStorage(STORAGE: any, schedule: Lecture[]): Promise<string[]> {
   const groups: string[] = [];
+  
   schedule.forEach((lecture) => {
     lecture.groups.forEach((group) => {
       if (!groups.includes(group)) {
@@ -49,7 +56,27 @@ export async function scheduleFetch(props: RouterProps): Promise<IFetchAndHandle
       }
     });
   });
-  await STORAGE.put('groups', JSON.stringify(groups));
 
-  return {schedule: schedule, groups: groups, updated: true, lastScheduleFetch: now};
+  await STORAGE.put('groups', JSON.stringify(groups));
+  return groups;
+}
+
+export async function scheduleFetch(props: RouterProps): Promise<IFetchAndHandleScheduleResult> {
+  const { env, api } = props;
+  const { STORAGE } = env;
+  const now = new Date().toISOString();
+
+  const cachedScheduleResult = await getCachedScheduleAndGroups(STORAGE);
+
+  if (cachedScheduleResult) {
+    return cachedScheduleResult;
+  }
+
+  const schedule = await fetchScheduleFromAPI(api);
+
+  await saveScheduleToStorage(STORAGE, schedule, now);
+
+  const groups = await saveGroupsToStorage(STORAGE, schedule);
+
+  return { schedule, groups, updated: true, lastScheduleFetch: now };
 }
