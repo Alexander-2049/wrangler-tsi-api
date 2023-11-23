1863c4bf29098ded3c1fbed173fbf1d265b4a46c
Alexander-2049 <a.alexander.2049@gmail.com>
2023-11-23 02:14:29 +0200

    changes controller logic

--- src/controllers/changesController.ts
+++ src/controllers/changesController.ts
@@ -1,6 +1,11 @@
 import { IRequest } from "itty-router";
 import { RouterProps } from "../types/RouterProps";
+import Changes from "../models/Changes";
+import { scheduleFetch } from "../utils/scheduleFetch";
 
-export default function changesController(request: IRequest, props: RouterProps) {
-
+export default async function changesController(request: IRequest, props: RouterProps) {
+  await scheduleFetch(props);
+  const { getChanges } = Changes;
+  const changes = await getChanges(props.env.STORAGE);
+  return new Response(JSON.stringify(changes), { status: 200, headers: { 'Content-Type': 'application/json' } });
 }
