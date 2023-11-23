55cf25278c25a3064de09abec559917159229b6b
Alexander-2049 <a.alexander.2049@gmail.com>
2023-11-23 02:13:57 +0200

    added changes route

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import { RouterProps } from './types/RouterProps';
 import fetchController from './controllers/fetchController';
 import scheduleController from './controllers/scheduleController';
 import groupsController from './controllers/groupsController';
+import changesController from './controllers/changesController';
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.get('/fetch', async (request, props: RouterProps) => await fetchControlle
 router.get('/schedule', async (request, props: RouterProps) => await scheduleController(request, props));
 router.get('/schedule/:group', async (request, props: RouterProps) => await scheduleController(request, props));
 router.get('/groups', async (request, props: RouterProps) => await groupsController(request, props));
+router.get('/changes', async (request, props: RouterProps) => await changesController(request, props));
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
